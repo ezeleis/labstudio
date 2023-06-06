2e53b5819da82c9288a0e51a372d834dbf8057f7
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-06-06 14:56:05 -0300

    feature(classes):add Cargo class

---
+++ src/Classes/Cargo.java
@@ -0,0 +1,12 @@
+package Classes;
+public class Cargo {
+    private String descricao;
+    private double salarioBase;
+    private Nivel nivel;
+
+    public Cargo(String descricao, double salarioBase, Nivel nivel) {
+        this.descricao = descricao;
+        this.salarioBase = salarioBase;
+        this.nivel = nivel;
+    }
+};
